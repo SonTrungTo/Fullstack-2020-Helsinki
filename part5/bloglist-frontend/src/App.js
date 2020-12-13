90ca82520037cf6bea8fec66a2e172c885f9dd74
SonTrungTo <son.trung.to@gmail.com>
2020-12-14 00:42:11 +0200

    5.1 solved!

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
-  }, [])
+    );
+  }, []);
 
-  return (
+  const blogsContent = () => (
     <div>
       <h2>blogs</h2>
+      { user && <p>{user.name} logged in</p> }
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
+
+  const loginForm = () => (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={({target}) => setUsername(target.value)} />
+        </div>
+        <div>
+          password
+          <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={({target}) => setPassword(target.value)} />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  );
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username, password
+      });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage(`${exception}`);
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 5000);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      { errorMessage && <p>{errorMessage}</p> }
+      { user === null ? loginForm() : blogsContent() }
+    </React.Fragment>
+  );
+};
 
 export default App
