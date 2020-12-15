035e3b7f0b76071dad40e5a2a531a34d83c11c37
SonTrungTo <son.trung.to@gmail.com>
2020-12-15 14:49:13 +0200

    fixed eslints errors. Prepare exercise 5.11-5.12

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,41 +1,41 @@
 import React, { useState } from 'react';
 
 export default function LoginForm({
-    loginUser
+	loginUser
 }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-    const handleLogin = event => {
-        event.preventDefault();
+	const handleLogin = event => {
+		event.preventDefault();
 
-        loginUser({username, password});
-        setUsername('');
-        setPassword('');
-    };
+		loginUser({ username, password });
+		setUsername('');
+		setPassword('');
+	};
 
-    return (
-    <div>
-        <h2>log in to application</h2>
-        <form onSubmit={handleLogin}>
-            <div>
+	return (
+		<div>
+			<h2>log in to application</h2>
+			<form onSubmit={handleLogin}>
+				<div>
                 username
-                <input
-                type='text'
-                name='username'
-                value={username}
-                onChange={({target}) => setUsername(target.value)} />
-            </div>
-            <div>
+					<input
+						type='text'
+						name='username'
+						value={username}
+						onChange={({ target }) => setUsername(target.value)} />
+				</div>
+				<div>
                 password
-                <input
-                type='password'
-                name='password'
-                value={password}
-                onChange={({target}) => setPassword(target.value)} />
-            </div>
-            <button type='submit'>login</button>
-        </form>
-    </div>
-    );
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={({ target }) => setPassword(target.value)} />
+				</div>
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	);
 }
--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -1,29 +1,31 @@
-import React, { useState, useImperativeHandle } from "react";
+import React, { useState, useImperativeHandle } from 'react';
 
 const Togglable = React.forwardRef((props, ref) => {
-    const [visible, setVisible] = useState(false);
-    const showWhenVisible = { display: visible ? '' : 'none' };
-    const hideWhenVisible = { display: visible ? 'none' : '' };
+	const [visible, setVisible] = useState(false);
+	const showWhenVisible = { display: visible ? '' : 'none' };
+	const hideWhenVisible = { display: visible ? 'none' : '' };
 
-    const toggleVisibility = () => {
-        setVisible(!visible);
-    };
+	const toggleVisibility = () => {
+		setVisible(!visible);
+	};
 
-    useImperativeHandle(ref, () => ({
-        toggleVisibility
-    }));
+	useImperativeHandle(ref, () => ({
+		toggleVisibility
+	}));
 
-    return (
-        <div>
-            <div style={hideWhenVisible}>
-                <button onClick={toggleVisibility}>{props.labelButton}</button>
-            </div>
-            <div style={showWhenVisible} id='create-blog-form'>
-                { props.children }
-                <button onClick={toggleVisibility}>cancel</button>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div style={hideWhenVisible}>
+				<button onClick={toggleVisibility}>{props.labelButton}</button>
+			</div>
+			<div style={showWhenVisible} id='create-blog-form'>
+				{ props.children }
+				<button onClick={toggleVisibility}>cancel</button>
+			</div>
+		</div>
+	);
 });
+
+Togglable.displayName = 'Togglable';
 
 export default Togglable;
