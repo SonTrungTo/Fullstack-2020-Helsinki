8bb221ecfc36508e39e6b1c62ffbcbee199bcf58
SonTrungTo <son.trung.to@gmail.com>
2020-12-20 01:02:57 +0200

    6.13-6.14 solved!

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import anecdotesService from '../services/anecdotes';
 import { createNewAnecdote } from '../reducers/anecdoteReducer';
 import { newAnecdoteMessage, removeMessage } from '../reducers/notificationReducer';
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch();
 
-    const addAnecdote = event => {
+    const addAnecdote = async event => {
         event.preventDefault();
         const content = event.target.anecdote.value;
         event.target.anecdote.value = '';
-        dispatch(createNewAnecdote(content));
-        dispatch(newAnecdoteMessage(content));
+        const newAnecdote = await anecdotesService.createNew(content);
+        dispatch(createNewAnecdote(newAnecdote));
+        dispatch(newAnecdoteMessage(newAnecdote.content));
         setTimeout(() => {
             dispatch(removeMessage());
         }, 5000);
