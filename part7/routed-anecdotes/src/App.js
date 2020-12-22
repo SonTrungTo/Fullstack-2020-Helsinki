0d6c23085bb7ca2f8cec00039e9e5eb82f08b7d1
SonTrungTo <son.trung.to@gmail.com>
2020-12-22 11:06:25 +0200

    7.5-7.6 done!

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -71,24 +71,31 @@ const CreateNew = (props) => {
         history.push('/');
     };
 
+    const handleReset = () => {
+        content.reset();
+        author.reset();
+        info.reset();
+    };
+
     return (
         <div>
             <h2>create a new anecdote</h2>
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input { ...content } />
+                    <input { ...content } reset='' />
                 </div>
                 <div>
                     author
-                    <input { ...author } />
+                    <input { ...author } reset='' />
                 </div>
                 <div>
                     url for more info
-                    <input { ...info } />
+                    <input { ...info } reset='' />
                 </div>
                 <button>create</button>
             </form>
+            <button onClick={handleReset}>reset</button>
         </div>
     );
 
