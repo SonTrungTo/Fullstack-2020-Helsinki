e1394a802e5475dec21ad7d48e2dd16393b662f0
SonTrungTo <son.trung.to@gmail.com>
2021-01-10 06:01:49 +0200

    9.22 in progress...

---
+++ part9/patientor/frontend/src/components/OccupationalHealthcare.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { OccupationalHealthcareEntry } from "../types";
+
+import { Card, Icon, CardHeader, CardContent,
+CardDescription } from "semantic-ui-react";
+
+const OccupationalHealthcare: React.FC<{ entry: OccupationalHealthcareEntry }>
+    = ({ entry }) => {
+        return (
+            <div style={{ width: '100%' }}>
+                <Card>
+                    <CardContent>
+                        <CardHeader>
+                            { entry.date }
+                            <span style={{ marginLeft: 20, marginRight: 20 }}>
+                                <Icon name='stethoscope' size='large' />
+                            </span>
+                            { entry.employerName }
+                        </CardHeader>
+                        <CardDescription>
+                            <p style={{ fontWeight: 'lighter' }}>
+                                { entry.description }
+                            </p>
+                        </CardDescription>
+                    </CardContent>
+                </Card>
+            </div>
+        );
+};
+
+export default OccupationalHealthcare;
--- part9/patientor/frontend/src/types.ts
+++ part9/patientor/frontend/src/types.ts
@@ -40,7 +40,7 @@ interface HospitalEntry extends BaseEntry {
   discharge: Discharge;
 }
 
-interface OccupationalHealthcareEntry extends BaseEntry {
+export interface OccupationalHealthcareEntry extends BaseEntry {
   type: 'OccupationalHealthcare';
   employerName: string;
   sickLeave?: SickLeave;
